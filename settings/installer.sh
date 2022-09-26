2cfdf65e378b5d48d90c8037fcf97654faf0d6d0
join93 <109458670+join93@users.noreply.github.com>
2022-09-26 13:16:59 +0300

    Add files via upload

--- settings/installer.sh
+++ settings/installer.sh
@@ -1,34 +1,12 @@
-#!/bin/sh
+init 2 
+cd /tmp 
+set -e  
+tar -xzf settings.tar.gz -C / 
+set +e 
+init 4; sleep 5; init 3 
 
-#wget -q "--no-check-certificate" https://raw.githubusercontent.com/join93/pro/main/settings/installer.sh -O - | /bin/sh
 
-version=1.0
-#############################################################
-TEMPATH=/tmp
 
-init 2
 
-echo ""
-# Download and install plugin
-cd /tmp
-set -e
-echo "===> Downloading And Installing settings plugin Please Wait ......"
-echo
-wget "https://raw.githubusercontent.com/join93/pro/main/settings/settings.tar.gz"
-tar -xzf settings.tar.gz -C /
-set +e
-rm -f settings.tar.gz
 
-init 4; sleep 5; init 3
 
-echo ""
-sync
-echo "##############################################################"
-echo "#         settings $version INSTALLED SUCCESSFULLY           #"
-echo "##############################################################"
-echo "**************************************************************"
-echo "##############################################################"
-echo "#              your Device will RESTART Now                  #"
-echo "##############################################################"
-
-exit 0
