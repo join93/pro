03a3b1a45d4b7b96605b25341265fc6fcf20b233
join93 <109458670+join93@users.noreply.github.com>
2023-01-17 10:38:46 +0200

    Add files via upload

---
+++ encryption/oscam-installer.sh
@@ -0,0 +1,10 @@
+echo " installing plugin please wait... "
+opkg install --force-overwrite  https://gitfront.io/r/eliesat/xeFZ1oNY38TL/pro/raw/encryption/enigma2-plugin-softcams-oscam_11.718-emu-r798_all.ipk
+wait
+sleep 2;
+exit 0
+
+
+
+
+
